886254393460077e0fd8bf8734d12252edebd364
SBiscBoi <80650186+SBiscBoi@users.noreply.github.com>
2023-03-19 01:08:54 -0500

    resize canvas added

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ from tkinter import filedialog, messagebox, colorchooser
 from PIL import Image, ImageDraw
 import PIL 
 
-WIDTH, HEIGHT = 400, 400
+DEFAULT_CANVAS_WIDTH, DEFAULT_CANVAS_HEIGHT = 400, 400
+WIDTH, HEIGHT = DEFAULT_CANVAS_WIDTH + 10, DEFAULT_CANVAS_HEIGHT + 10
 DEFAULT_BG = (255, 255, 255) # White
 
 class EmeraldPaintGUI:
@@ -17,6 +18,8 @@ class EmeraldPaintGUI:
 
         self.brushWidth = 10.0 # initial brush width
         self.currentColor = "#000000" # initial brush color
+        self.canvasWidth = DEFAULT_CANVAS_WIDTH
+        self.canvasHeight = DEFAULT_CANVAS_HEIGHT
 
         # Menu bar
         self.menuBar = Menu(self.root)
@@ -27,21 +30,26 @@ class EmeraldPaintGUI:
         self.fileMenu.add_command(label="Save Paint", command=self.save)
         self.menuBar.add_cascade(label="File", menu=self.fileMenu)
 
+        # Canvas menu
+        self.canvasmenu = Menu(self.menuBar, tearoff=0)
+        self.canvasmenu.add_command(label="Resize Canvas", command=self.resizeCanvas)
+        self.menuBar.add_cascade(label="Canvas", menu=self.canvasmenu)
+
         # Brush menu
         self.brushmenu = Menu(self.menuBar, tearoff=0)
         self.brushmenu.add_command(label="Change Color", command=self.changeColor)
         # Brush Size will open a new small window, may change later
-        self.brushmenu.add_cascade(label="Change Size", command=self.changeBrushSize)
+        self.brushmenu.add_cascade(label="Change Size", command=self.resizeBrush)
         self.menuBar.add_cascade(label="Brush", menu=self.brushmenu)
         
         self.root.config(menu=self.menuBar)
 
-        self.canvas = Canvas(self.root, width=WIDTH-10, height=HEIGHT-10, bg=self.convinceRGB(DEFAULT_BG))
+        self.canvas = Canvas(self.root, width=DEFAULT_CANVAS_WIDTH, height=DEFAULT_CANVAS_HEIGHT, bg=self.convinceRGB(DEFAULT_BG))
         self.canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
         self.canvas.pack()
         self.canvas.bind("<B1-Motion>", self.paint) # any mouse movement inside canvas or LMB will trigger the paint method
 
-        self.image = PIL.Image.new("RGB", (WIDTH, HEIGHT), DEFAULT_BG)
+        self.image = PIL.Image.new("RGB", (DEFAULT_CANVAS_WIDTH, DEFAULT_CANVAS_HEIGHT), DEFAULT_BG)
         self.draw = ImageDraw.Draw(self.image)
 
 
@@ -81,7 +89,7 @@ class EmeraldPaintGUI:
             self.currentColor = chosenColor
 
     # Open the window to change brush size
-    def changeBrushSize(self):
+    def resizeBrush(self):
         self.brushSizeWindow = Toplevel(self.root)
         self.brushSizeWindow.title("Change Brush Size")
         self.brushSizeWindow.geometry("140x30")
@@ -125,8 +133,65 @@ class EmeraldPaintGUI:
     def convinceRGB(self, rgb):
         return "#%02x%02x%02x" % rgb
 
+    # Center the canvas within root window
     def centerCanvas(self, event):
         self.canvas.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    # Open window to input and change canvas size
+    def resizeCanvas(self):
+        self.canvasSizeWindow = Toplevel(self.root)
+        self.canvasSizeWindow.title("Change Canvas Size")
+        self.canvasSizeWindow.geometry("150x70")
+        self.canvasSizeWindow.resizable(0,0)
+        self.canvasSizeWindow.attributes("-topmost", True)
+        self.canvasSizeWindow.attributes("-toolwindow", True)
+
+        self.canvasSizeWindow.grid_rowconfigure(3)
+        self.canvasSizeWindow.grid_columnconfigure(2)
+
+        self.canvasSizeWLabel = Label(self.canvasSizeWindow, text="Width:", height=1)
+        self.canvasSizeWLabel.grid(row=0, column=0, sticky="nsew", padx=20)
+
+        self.canvasSizeWInput = Entry(self.canvasSizeWindow, width=6)
+        self.canvasSizeWInput.grid(row=0, column=1, sticky="nsew")
+        self.canvasSizeWInput.insert(0, str(self.canvasWidth))
+                
+        self.canvasSizeHLabel = Label(self.canvasSizeWindow, text="Height:", height=1)
+        self.canvasSizeHLabel.grid(row=1, column=0, sticky="nsew")
+
+        self.canvasSizeHInput = Entry(self.canvasSizeWindow, width=6)
+        self.canvasSizeHInput.grid(row=1, column=1, sticky="nsew")
+        self.canvasSizeHInput.insert(0, str(self.canvasHeight))
+
+        self.canvasSizeSetButton = Button(self.canvasSizeWindow, text="Submit", command=lambda: self.setCanvasSize(self.canvasSizeWInput.get(), self.canvasSizeHInput.get())) # verifty input is acceptable, set size
+        self.canvasSizeSetButton.grid(row=2, column=0, sticky="nsew", columnspan=2, padx=50)
+
+    # Verify input, change window size if needed, resize canvas
+    def setCanvasSize(self, width, height):
+        try:
+            width = int(width)
+            height = int(height)
+        except ValueError:
+            messagebox.showerror("Canvas Resize Error", "Input must be a number.")
+            self.canvasSizeWindow.destroy()
+
+        answer = messagebox.askyesnocancel("Clear Warning", "WARNING: Resizing the canvas will clear the current painting.  Are you sure you want to resize?", parent=self.canvasSizeWindow)
+        if answer != None:
+            if answer:
+                self.clear()
+                # If window is smaller than canvas + padding, resize it
+                print(self.root.winfo_width())
+                print(width + 10)
+                print(width + 10 > self.root.winfo_width())
+                if width + 10 > self.root.winfo_width() or height + 10 > self.root.winfo_height():
+                    self.root.minsize(width=width+10, height=height+10)
+                self.canvas.config(width=width, height=height)
+                self.image = PIL.Image.new("RGB", (width, height), DEFAULT_BG)
+                self.draw = ImageDraw.Draw(self.image)
+                self.centerCanvas(1) # takes in "event" but we can just toss garbage in here
+            self.canvasSizeWindow.destroy() # should kill this child window as a result
+
+
 
 
 EmeraldPaintGUI()
